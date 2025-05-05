7a8524460aba7c59641e1441ead5c4d8ce111920
Gipnokot <shootinputin007@gmail.com>
2025-05-06 00:41:42 +0500

    Add confirmation/pass_reset email sending with sidekiq

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem "rails", "~> 7.2.2", ">= 7.2.2.1"
 gem "pg", "~> 1.1"
 gem "puma", ">= 5.0"
+gem "sidekiq", "~> 8.0", ">= 8.0.3"
 
 # --- Frontend ---
 gem "importmap-rails"
---
+++ app/jobs/devise_mailer_job.rb
@@ -0,0 +1,31 @@
+class DeviseMailerJob < ApplicationJob
+  queue_as :mailers
+  retry_on StandardError, wait: 5.minutes, attempts: 3
+
+  def perform(user_id, type, *args)
+    user = User.find_by(id: user_id)
+    unless user
+      Rails.logger.error "DeviseMailerJob: User not found with id=#{user_id}"
+      return
+    end
+
+    begin
+      Rails.logger.info "DeviseMailerJob: Sending #{type} email to #{user.email}"
+      case type.to_sym
+      when :confirmation
+        Devise::Mailer.confirmation_instructions(user, *args).deliver_now
+      when :reset_password
+        Devise::Mailer.reset_password_instructions(user, *args).deliver_now
+      when :unlock
+        Devise::Mailer.unlock_instructions(user, *args).deliver_now
+      else
+        Rails.logger.error "DeviseMailerJob: Unknown email type #{type}"
+        return
+      end
+      Rails.logger.info "DeviseMailerJob: Successfully sent #{type} email to #{user.email}"
+    rescue StandardError => e
+      Rails.logger.error "DeviseMailerJob: Failed to send #{type} email to #{user.email}: #{e.message}"
+      raise
+    end
+  end
+end
