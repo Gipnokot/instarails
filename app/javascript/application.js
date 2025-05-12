5cb73a9c32dbd67cacbbaa984cf83ac49615145c
Gipnokot <shootinputin007@gmail.com>
2025-05-12 18:08:58 +0500

    fix project

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import { createIcons, icons } from "lucide"
-
-document.addEventListener("DOMContentLoaded", () => {
-  createIcons({ icons });
-});
